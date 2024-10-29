253e5c723952abb0fc645e7a2e7a8d29b4541d29
Martin-C1 <mcahue4@gmail.com>
2024-10-29 14:46:17 -0500

    Update M6-teamassignment.py

--- M6-teamassignment.py
+++ M6-teamassignment.py
@@ -1,4 +1,4 @@
-#the code should first print "guess the number that is 1 to 10"
+e#the code should first print "guess the number that is 1 to 10"
 #While doing so the code should choose a random number from a generator, 1 to 100
 #The user will then type a number of their choosing
 #IF the user picks a higher number than the number chosen print "Please guess lower"
@@ -12,9 +12,9 @@ guess = int(guess)
 if guess < 1 or guess > 10:
     print ("Your number must be between 1 or 10")
 if guess < random_number:
-    print("Please higher number")
+    print("Please enter higher number")
 elif guess > random_number:
-    print ("Please lower number")
+    print ("Please enter lower number")
 else:
     print("congratulations you did it")
 
